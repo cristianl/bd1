37e61a0f963fbc52829f4bbb820c4ffa1ee09433
Cristian Lorsson <contact@cristian.im>
2026-06-07 14:15:53 -0300

    Avoid bottles for deps

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ sudo chown -R $USER /usr/local/Cellar
 chmod u+w /usr/local/Cellar
 
 export MACOSX_DEPLOYMENT_TARGET="13.0"
+brew install --only-dependencies --build-from-source zstd
 brew install --build-from-source zstd
