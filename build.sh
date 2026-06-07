ff4d789d17aca914393de09441504a213af0d535
Cristian Lorsson <contact@cristian.im>
2026-06-07 14:42:08 -0300

    set CMAKE_OSX_DEPLOYMENT_TARGET
    Updated build script to set CMAKE_OSX_DEPLOYMENT_TARGET and clean up CMake.

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ sudo chown -R $USER /usr/local/Cellar
 chmod u+w /usr/local/Cellar
 
 export MACOSX_DEPLOYMENT_TARGET="13.0"
-brew install --build-from-source $(brew deps zstd) zstd
+export CMAKE_OSX_DEPLOYMENT_TARGET="13.0"
+brew install --build-from-source $(brew deps --installed zstd) zstd
+sudo rm -r /usr/local/Cellar/cmake
+brew cleanup
 brew autoremove
