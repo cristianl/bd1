7081e89c6de989fb63223bb4b5110ddc98cd83b0
Cristian Lorsson <contact@cristian.im>
2026-06-07 16:02:46 -0300

    cflags
    Updated macOS deployment target to 13.5 and added architecture flags.

--- build.sh
+++ build.sh
@@ -5,8 +5,13 @@ sudo mkdir /usr/local/Cellar
 sudo chown -R $USER /usr/local/Cellar
 chmod u+w /usr/local/Cellar
 
-export MACOSX_DEPLOYMENT_TARGET="13.0"
-export CMAKE_OSX_DEPLOYMENT_TARGET="13.0"
+export MACOSX_DEPLOYMENT_TARGET="13.5"
+export CMAKE_OSX_DEPLOYMENT_TARGET="13.5"
+export CFLAGS="-arch x86_64 -march=sandybridge -mtune=sandybridge -Os"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="$CFLAGS"
+# export CMAKE_OSX_ARCHITECTURES="x86_64"
+
 brew install --build-from-source $(brew deps zstd) zstd
 sudo rm -r /usr/local/Cellar/cmake
 brew cleanup
