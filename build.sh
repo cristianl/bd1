5d3f9b49e86ce26f708b6e1b1e4fca52419cae34
Cristian Lorsson <contact@cristian.im>
2026-06-07 14:43:39 -0300

    .

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ chmod u+w /usr/local/Cellar
 
 export MACOSX_DEPLOYMENT_TARGET="13.0"
 export CMAKE_OSX_DEPLOYMENT_TARGET="13.0"
-brew install --build-from-source $(brew deps --installed zstd) zstd
+brew install --build-from-source $(brew deps zstd) zstd
 sudo rm -r /usr/local/Cellar/cmake
 brew cleanup
 brew autoremove
