4ba19229e80bbeb1b28b2b78651b6e750a4b0c79
Cristian Lorsson <contact@cristian.im>
2026-06-07 20:44:40 -0300

    .
    [skip ci]

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 #!/bin/zsh
 
-sudo mv /usr/local/Cellar /usr/local/Cellar_bak
-sudo mkdir /usr/local/Cellar
-sudo chown -R $USER /usr/local/Cellar
-chmod u+w /usr/local/Cellar
+brew deps mpv
 
-export MACOSX_DEPLOYMENT_TARGET="13.5"
-export CMAKE_OSX_DEPLOYMENT_TARGET="13.5"
-export CFLAGS="-arch x86_64 -march=sandybridge -mtune=sandybridge -Os"
-export CXXFLAGS="$CFLAGS"
-export LDFLAGS="$CFLAGS"
-# export CMAKE_OSX_ARCHITECTURES="x86_64"
+# sudo mv /usr/local/Cellar /usr/local/Cellar_bak
+# sudo mkdir /usr/local/Cellar
+# sudo chown -R $USER /usr/local/Cellar
+# chmod u+w /usr/local/Cellar
 
-PATH="$PATH"
+# export MACOSX_DEPLOYMENT_TARGET="13.5"
+# export CMAKE_OSX_DEPLOYMENT_TARGET="13.5"
+# export CFLAGS="-arch x86_64 -march=sandybridge -mtune=sandybridge -Os"
+# export CXXFLAGS="$CFLAGS"
+# export LDFLAGS="$CFLAGS"
+# # export CMAKE_OSX_ARCHITECTURES="x86_64"
 
-brew analytics off
-brew install --build-from-source $(brew deps zstd) zstd
-brew cleanup
-brew autoremove
-ls -al /usr/local/Cellar
-ls -al /usr/local/Cellar/cmake
-sudo rm -r /usr/local/Cellar/cmake
+# PATH="$PATH"
+
+# brew analytics off
+# brew install --build-from-source $(brew deps zstd) zstd
+# brew cleanup
+# brew autoremove
+# ls -al /usr/local/Cellar
+# ls -al /usr/local/Cellar/cmake
+# sudo rm -r /usr/local/Cellar/cmake
 
 # when reusing this, remember: zstd works in next task because it's reinstalled here 
